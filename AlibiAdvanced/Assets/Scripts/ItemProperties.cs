94c7271f657c612f520dd90d8c0dedb503f5b133
M1nKWuZ3 <a14725831116@163.com>
2026-02-15 03:31:11 -0500

    Commit-0215-0331

---
+++ AlibiAdvanced/Assets/Scripts/ItemProperties.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+
+namespace Alibi.Inspections
+{
+    public class ItemProperties : MonoBehaviour
+    {
+        public string itemName;
+        public string itemDescription;
+
+    }
+}
